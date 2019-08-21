1d09f2a07074c69b6c9ba5538158345557d5ba2f
Raúl González Portillo <raulgonzalezportillo@gmail.com>
2019-08-21 17:50:46 -0500

    21 de Agosto de 2019

---
+++ ClaseBienvenida.java
@@ -0,0 +1,8 @@
+public class ClaseBienvenida {
+    public static void main (String args[]) {
+	MensajeSaludo Objeto1 = new MensajeSaludo ();
+	Objeto1.saludos();
+	MensajeSaludo Objeto2 = new MensajeSaludo ();
+	Objeto2.otrosaludo();
+    }
+}
---
+++ MensajeSaludo.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class MensajeSaludo {
+    public void saludos() {
+        JOptionPane.showMessageDialog (null, "Saludos al grupo POO", "Título Ventana", JOptionPane.INFORMATION_MESSAGE);
+    }
+    public void otrosaludo() {
+	System.out.println ("Bueno bai");
+    }
+}
