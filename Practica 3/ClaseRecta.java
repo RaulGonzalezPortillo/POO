17833e487849c837430c7809e855ae00448a4893
Raúl González Portillo <raulgonzalezportillo@gmail.com>
2019-09-04 17:33:14 -0500

    4 de Septiembre de 2019

--- Practica 3/ClaseRecta.java
+++ Practica 3/ClaseRecta.java
@@ -1,3 +1,30 @@
-public class ClasePesca {
-  static float m, b;
+public class ClaseRecta {
+  float m, b;
+  public void calcularPendiente (float X1, float Y1, float X2, float Y2) {
+    m = (Y2 - Y1) / (X2 - X1);
+  }
+  public float getPendiente () {
+    return m;
+  }
+  public void calcularOrdenada (float X1, float Y1, float X2, float Y2) {
+    b = Y1 - m * X1;
+  }
+  public float getOrdenada () {
+    return b;
+  }
+  public void interseccion (float m2, float b2) {
+    float x, y, Divisor;
+    if (m == m2)
+      System.out.println ("Las rectas son paralelas");
+    else
+    {
+      Divisor = m - m2;
+      x = (b2 - b) / Divisor;
+      y = (m * x) + b;
+      System.out.println ("Las rectas se intersectan en " + x + ", " + y);
+    }
+  }
+  public double angulo (float m2) {
+    return Math.toDegrees (Math.atan ((m2 - m) / (1 + (m2 * m))));
+  }
 }
--- Practica 3/ContenedorClaseRecta.java
+++ Practica 3/ContenedorClaseRecta.java
@@ -1,17 +1,37 @@
 import java.util.Scanner;
 
 public class ContenedorClaseRecta {
-  static Scanner scan = new Scanner(System.in);
+  static Scanner scan = new Scanner (System.in);
   public static void main (String[] args) {
-    ClaseRecta objetoRecta = new ClaseRecta ();
-    System.out.println("Ingresa el valor de X1: ");
-    int X1 = scan.nextInt();
-    System.out.println("Ingresa el valor de Y1: ");
-    int Y1 = scan.nextInt();
-    System.out.println("Ingresa el valor de X2: ");
-    int X2 = scan.nextInt();
-    System.out.println("Ingresa el valor de Y2: ");
-    int Y2 = scan.nextInt();
-    System.out.println()
+    ClaseRecta objetoRecta1 = new ClaseRecta ();
+    ClaseRecta objetoRecta2 = new ClaseRecta ();
+    System.out.println ("Ingresa el valor de X1 de la recta 1: ");
+    float X1R1 = scan.nextFloat ();
+    System.out.println ("Ingresa el valor de Y1 de la recta 1: ");
+    float Y1R1 = scan.nextFloat ();
+    System.out.println ("Ingresa el valor de X2 de la recta 1: ");
+    float X2R1 = scan.nextFloat ();
+    System.out.println ("Ingresa el valor de Y2 de la recta 1: ");
+    float Y2R1 = scan.nextFloat ();
+    System.out.println ("Ingresa el valor de X1 de la recta 2: ");
+    float X1R2 = scan.nextFloat ();
+    System.out.println ("Ingresa el valor de Y1 de la recta 2: ");
+    float Y1R2 = scan.nextFloat ();
+    System.out.println ("Ingresa el valor de X2 de la recta 2: ");
+    float X2R2 = scan.nextFloat ();
+    System.out.println ("Ingresa el valor de Y2 de la recta 2: ");
+    float Y2R2 = scan.nextFloat ();
+    objetoRecta1.calcularPendiente (X1R1, Y1R1, X2R1, Y2R1);
+    objetoRecta1.calcularOrdenada (X1R1, Y1R1, X2R1, Y2R1);
+    System.out.println ("La ecuación de la recta 1 es: " + objetoRecta1.getPendiente () + "x + " + objetoRecta1.getOrdenada ());
+    System.out.println ("Su pendiente es: " + objetoRecta1.getPendiente ());
+    System.out.println ("Corta al eje y en: " + objetoRecta1.getOrdenada ());
+    objetoRecta2.calcularPendiente (X1R2, Y1R2, X2R2, Y2R2);
+    objetoRecta2.calcularOrdenada (X1R2, Y1R2, X2R2, Y2R2);
+    System.out.println ("La ecuación de la recta 2 es: " + objetoRecta2.getPendiente () + "x + " + objetoRecta2.getOrdenada ());
+    System.out.println ("Su pendiente es: " + objetoRecta2.getPendiente ());
+    System.out.println ("Corta al eje y en: " + objetoRecta2.getOrdenada ());
+    objetoRecta1.interseccion (objetoRecta2.getPendiente (), objetoRecta2.getOrdenada ());
+    System.out.println ("El angulo entre rectas es: " + objetoRecta1.angulo (objetoRecta2.getPendiente ()));
   }
 }
