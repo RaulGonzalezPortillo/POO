ae46882510709cfbe20ff820022a385fa949ecef
Raúl González Portillo <raulgonzalezportillo@gmail.com>
2019-09-12 18:04:35 -0500

    12 de Septiembre de 2019

--- Práctica 9/ClaseMediaFrecuencia.java
+++ Práctica 9/ClaseMediaFrecuencia.java
@@ -1,16 +1,94 @@
 public class ClaseMediaFrecuencia
 {
-  double MediaFrecuencia;
-  public void calcularMedia (int [] Arreglo, int Cantidad)
+  double MediaFrecuencia, Mediana;
+  int [] Arreglo2, Frecuencias;
+  int i, j, Temp, CantidadDividida;
+  boolean Par;
+  public int [] Ordenar (int [] Arreglo, int Cantidad)
   {
-    for (int i = 0; i < Cantidad; i ++)
+    for (i = 0; i < Cantidad - 1; i ++)
     {
-      MediaFrecuencia += Arreglo [i];
+      for (j = 0; j < (Cantidad - i - 1); j ++)
+      {
+        if (Arreglo [j] > Arreglo [j + 1])
+        {
+          Temp = Arreglo [j];
+          Arreglo [j] = Arreglo [j + 1];
+          Arreglo [j + 1] = Temp;
+        }
+      }
+    }
+    return Arreglo;
+  }
+  public void Conversion (int [] Arreglo, int Cantidad)
+  {
+    Arreglo2 = new int [Cantidad + 1];
+    Frecuencias = new int [Cantidad];
+    Arreglo2 [0] = Arreglo [0];
+    Frecuencias [0] = 1;
+    j = 0;
+    for (i = 1; i < Cantidad; i ++)
+    {
+      if (Arreglo [i] != Arreglo [i - 1])
+      {
+        j ++;
+        Arreglo2 [j] = Arreglo [i];
+        Frecuencias [j] = 1;
+      }
+      else
+      {
+        (Frecuencias [j]) ++;
+      }
+    }
+    Arreglo2 [i] = 0;
+  }
+  public int [] getArreglo2 ()
+  {
+    return Arreglo2;
+  }
+  public int [] getFrecuencias ()
+  {
+    return Frecuencias;
+  }
+  public void imprimirTabla (int [] Arreglo2, int [] Frecuencias)
+  {
+    System.out.println ("Tabla de frecuencias:");
+    for (i = 0; Arreglo2 [i] != 0 ; i ++)
+    {
+      System.out.println (Arreglo2 [i] + " " + Frecuencias [i]);
+    }
+  }
+  public void calcularMediaFrecuencia (int [] Arreglo2, int Cantidad)
+  {
+    for (i = 0; Arreglo2[i] != 0; i ++)
+    {
+      MediaFrecuencia += (Arreglo2 [i] * Frecuencias [i]);
     }
     MediaFrecuencia /= Cantidad;
   }
-  public double getMedia ()
+  public double getMediaFrecuencia ()
   {
     return MediaFrecuencia;
+  }
+  public void calcularMediana (int [] Arreglo, int Cantidad)
+  {
+    if (Cantidad % 2 == 0)
+    {
+      Par = true;
+    }
+    CantidadDividida = Cantidad / 2;
+    if (Par)
+    {
+      System.out.println ((Arreglo [CantidadDividida - 1]) + " " + (Arreglo [CantidadDividida]));
+      Mediana = (double)(((Arreglo [CantidadDividida - 1]) + (Arreglo [CantidadDividida]) / 2));
+    }
+    else
+    {
+      Mediana = Arreglo [CantidadDividida];
+    }
+  }
+  public double getMediana ()
+  {
+    return Mediana;
   }
 }
--- Práctica 9/ContenedorClaseMediaFrecuencia.java
+++ Práctica 9/ContenedorClaseMediaFrecuencia.java
@@ -5,33 +5,27 @@ public class ContenedorClaseMediaFrecuencia
   static Scanner scan = new Scanner (System.in);
   public static void main (String[] args)
   {
-    int Lector, Temp;
-    ClaseMediaFrecuencia objetoMedia = new ClaseMediaFrecuencia ();
+    int Lector, Temp, i;
     System.out.println ("Ingrese la cantidad de números a leer");
     int Cantidad = scan.nextInt ();
+    ClaseMediaFrecuencia objetoMedia = new ClaseMediaFrecuencia ();
     int Arreglo [] = new int [Cantidad];
+    int Arreglo2 [] = new int [Cantidad];
+    int Frecuencias [] = new int [Cantidad];
     System.out.println ("Ingrese los siguientes valores");
-    System.out.println ("Valor 1:");
-    Arreglo [0] = scan.nextInt ();
-    for (int i = 1; i < Cantidad; i ++)
+    for (i = 0; i < Cantidad; i ++)
     {
       System.out.println ("Valor " +  (i + 1) + ":");
       Arreglo [i] = scan.nextInt ();
-      for (int j = i - 1 ; j < 0; j ++)
-      {
-        if (Arreglo [j] > Arreglo [i])
-        {
-          Temp = Arreglo [j];
-          Arreglo [j] = Arreglo [i];
-          Arreglo [i] = Temp;
-        }
-      }
     }
-    for (int i = 0; i < Cantidad; i ++)
-    {
-      System.out.println (Arreglo [i]);
-    }
-    objetoMedia.calcularMedia (Arreglo, Cantidad);
-    System.out.println ("La media aritmética es = " + objetoMedia.getMedia ());
+    Arreglo = objetoMedia.Ordenar (Arreglo, Cantidad);
+    objetoMedia.Conversion (Arreglo, Cantidad);
+    Arreglo2 = objetoMedia.getArreglo2 ();
+    Frecuencias = objetoMedia.getFrecuencias ();
+    objetoMedia.imprimirTabla (Arreglo2, Frecuencias);
+    objetoMedia.calcularMediaFrecuencia (Arreglo2, Cantidad);
+    System.out.println ("La media aritmética es = " + objetoMedia.getMediaFrecuencia ());
+    objetoMedia.calcularMediana (Arreglo, Cantidad);
+    System.out.println ("La mediana es = " + objetoMedia.getMediana ());
   }
 }
